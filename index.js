95888e981c6a75c5ebc290e895bf0eb00b40c4e9
Chirag <chiragdhawan2003@gmail.com>
2025-08-29 11:06:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.post("/bfhl", (req, res) => {
+  try {
+    const { data } = req.body;
+
+    const evens = [];
+    const odds = [];
+    const alphabets = [];
+    const specials = [];
+    let sum = 0;
+
+    data.forEach(item => {
+      if (!isNaN(item)) {
+        let num = parseInt(item);
+        if (num % 2 === 0) evens.push(item);
+        else odds.push(item);
+        sum += num;
+      } else if (/^[a-zA-Z]+$/.test(item)) {
+        alphabets.push(item.toUpperCase());
+      } else {
+        specials.push(item);
+      }
+    });
+
+    // Reverse + alternating caps for alphabets
+    let concat = alphabets.join("");
+    let rev = concat.split("").reverse();
+    let altCaps = rev.map((ch, i) =>
+      i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()
+    ).join("");
+
+    res.status(200).json({
+      is_success: true,
+      user_id: "Chirag_Dhawan_21072003",   // change this to your name + dob
+      email: "chiradg,dhawan2022@vitstudent.ac.in", // replace
+      roll_number: "22BCE1080",             // replace
+      odd_numbers: odds,
+      even_numbers: evens,
+      alphabets,
+      special_characters: specials,
+      sum: sum.toString(),
+      concat_string: altCaps
+    });
+  } catch (err) {
+    res.status(500).json({ is_success: false, error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
